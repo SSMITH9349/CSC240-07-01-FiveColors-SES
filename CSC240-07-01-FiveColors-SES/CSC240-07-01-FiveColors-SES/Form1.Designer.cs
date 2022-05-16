37334df246ea437e4dd6c10bd4c7dec710ad9e6c
Sara Smith <SSMITH9349@my.sullivan.edu>
2022-05-16 18:12:48 -0400

    Completed Buttons and code

--- CSC240-07-01-FiveColors-SES/CSC240-07-01-FiveColors-SES/Form1.Designer.cs
+++ CSC240-07-01-FiveColors-SES/CSC240-07-01-FiveColors-SES/Form1.Designer.cs
@@ -30,36 +30,104 @@
         {
             this.UxTitleLabel = new System.Windows.Forms.Label();
             this.UxManualLabel = new System.Windows.Forms.Label();
+            this.UxBlueRadioButton = new System.Windows.Forms.RadioButton();
+            this.UxRedRadioButton = new System.Windows.Forms.RadioButton();
+            this.UxPurpleRadioButton = new System.Windows.Forms.RadioButton();
+            this.UxYellowRadioButton = new System.Windows.Forms.RadioButton();
+            this.UxGreenRadioButton = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // UxTitleLabel
             // 
             this.UxTitleLabel.AutoSize = true;
-            this.UxTitleLabel.Font = new System.Drawing.Font("Baskerville Old Face", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.UxTitleLabel.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.UxTitleLabel.Location = new System.Drawing.Point(271, 27);
             this.UxTitleLabel.Name = "UxTitleLabel";
-            this.UxTitleLabel.Size = new System.Drawing.Size(186, 26);
+            this.UxTitleLabel.Size = new System.Drawing.Size(239, 34);
             this.UxTitleLabel.TabIndex = 0;
             this.UxTitleLabel.Text = "Color Generator:";
             // 
             // UxManualLabel
             // 
             this.UxManualLabel.AutoSize = true;
+            this.UxManualLabel.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.UxManualLabel.Location = new System.Drawing.Point(211, 97);
             this.UxManualLabel.Name = "UxManualLabel";
-            this.UxManualLabel.Size = new System.Drawing.Size(294, 20);
+            this.UxManualLabel.Size = new System.Drawing.Size(359, 23);
             this.UxManualLabel.TabIndex = 1;
             this.UxManualLabel.Text = "Please Choose Background Color Manually:";
+            // 
+            // UxBlueRadioButton
+            // 
+            this.UxBlueRadioButton.AutoSize = true;
+            this.UxBlueRadioButton.Location = new System.Drawing.Point(34, 140);
+            this.UxBlueRadioButton.Name = "UxBlueRadioButton";
+            this.UxBlueRadioButton.Size = new System.Drawing.Size(85, 24);
+            this.UxBlueRadioButton.TabIndex = 2;
+            this.UxBlueRadioButton.TabStop = true;
+            this.UxBlueRadioButton.Text = "<-- Blue";
+            this.UxBlueRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // UxRedRadioButton
+            // 
+            this.UxRedRadioButton.AutoSize = true;
+            this.UxRedRadioButton.Location = new System.Drawing.Point(161, 186);
+            this.UxRedRadioButton.Name = "UxRedRadioButton";
+            this.UxRedRadioButton.Size = new System.Drawing.Size(82, 24);
+            this.UxRedRadioButton.TabIndex = 3;
+            this.UxRedRadioButton.TabStop = true;
+            this.UxRedRadioButton.Text = "<-- Red";
+            this.UxRedRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // UxPurpleRadioButton
+            // 
+            this.UxPurpleRadioButton.AutoSize = true;
+            this.UxPurpleRadioButton.Location = new System.Drawing.Point(307, 140);
+            this.UxPurpleRadioButton.Name = "UxPurpleRadioButton";
+            this.UxPurpleRadioButton.Size = new System.Drawing.Size(98, 24);
+            this.UxPurpleRadioButton.TabIndex = 4;
+            this.UxPurpleRadioButton.TabStop = true;
+            this.UxPurpleRadioButton.Text = "<-- Purple";
+            this.UxPurpleRadioButton.UseVisualStyleBackColor = true;
+            this.UxPurpleRadioButton.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // UxYellowRadioButton
+            // 
+            this.UxYellowRadioButton.AutoSize = true;
+            this.UxYellowRadioButton.Location = new System.Drawing.Point(459, 186);
+            this.UxYellowRadioButton.Name = "UxYellowRadioButton";
+            this.UxYellowRadioButton.Size = new System.Drawing.Size(99, 24);
+            this.UxYellowRadioButton.TabIndex = 5;
+            this.UxYellowRadioButton.TabStop = true;
+            this.UxYellowRadioButton.Text = "<-- Yellow";
+            this.UxYellowRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // UxGreenRadioButton
+            // 
+            this.UxGreenRadioButton.AutoSize = true;
+            this.UxGreenRadioButton.Location = new System.Drawing.Point(610, 140);
+            this.UxGreenRadioButton.Name = "UxGreenRadioButton";
+            this.UxGreenRadioButton.Size = new System.Drawing.Size(95, 24);
+            this.UxGreenRadioButton.TabIndex = 6;
+            this.UxGreenRadioButton.TabStop = true;
+            this.UxGreenRadioButton.Text = "<-- Green";
+            this.UxGreenRadioButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UxGreenRadioButton);
+            this.Controls.Add(this.UxYellowRadioButton);
+            this.Controls.Add(this.UxPurpleRadioButton);
+            this.Controls.Add(this.UxRedRadioButton);
+            this.Controls.Add(this.UxBlueRadioButton);
             this.Controls.Add(this.UxManualLabel);
             this.Controls.Add(this.UxTitleLabel);
             this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,5 +137,10 @@
 
         private Label UxTitleLabel;
         private Label UxManualLabel;
+        private RadioButton UxBlueRadioButton;
+        private RadioButton UxRedRadioButton;
+        private RadioButton UxPurpleRadioButton;
+        private RadioButton UxYellowRadioButton;
+        private RadioButton UxGreenRadioButton;
     }
 }
--- CSC240-07-01-FiveColors-SES/CSC240-07-01-FiveColors-SES/Form1.cs
+++ CSC240-07-01-FiveColors-SES/CSC240-07-01-FiveColors-SES/Form1.cs
@@ -6,5 +6,29 @@ namespace CSC240_07_01_FiveColors_SES
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (UxBlueRadioButton.Checked)
+                this.BackColor = Color.Blue;
+            else if (UxRedRadioButton.Checked)
+                this.BackColor = Color.Red;
+            else if (UxGreenRadioButton.Checked)
+                this.BackColor = Color.Green;
+            else if (UxPurpleRadioButton.Checked)
+                this.BackColor = Color.Purple;
+            else if (UxYellowRadioButton.Checked)
+                this.BackColor = Color.Yellow;
+            else (UxBlueRadioButton.Checked) = false;                
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+        }
     }
 }
